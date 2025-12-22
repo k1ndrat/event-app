3072c5fe9cb6365134fefbdb52d9f657989ce02c
k1ndrat <rubalkanaspining@gmail.com>
2025-12-22 15:52:39 +0200

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
-      }),
-    }),
-    AuthModule,
-    UserModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
